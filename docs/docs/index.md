fd7ca95d7a3a55c67bdcd3fdb07a09a28bd9388e
Darshan31Patel <darshan.h3112@gmail.com>
2024-06-14 15:01:28 +0530

    initial commit

---
+++ docs/docs/index.md
@@ -0,0 +1,10 @@
+# DiamondPricePrediction documentation!
+
+## Description
+
+Diamond price prediction based on given features.
+
+## Commands
+
+The Makefile contains the central entry points for common tasks related to this project.
+
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Machine Learning project to predict Diamond price based on given features.',
+    author='Darshan',
+    license='MIT',
+)
